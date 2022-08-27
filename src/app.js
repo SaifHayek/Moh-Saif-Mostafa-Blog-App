773eca1a401e45afe9cb754b2e923b51c40bfcdd
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-08-27 18:51:27 +0300

    create controllers, router and query to select all posts and thire categories from database and send them to front side

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const compression = require('compression');
+const router = require('./router');
 
 const app = express();
 
@@ -11,7 +12,6 @@ app.use(compression());
 // to parse body of post request || put request
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use(express.static('public'));
-
+app.use(router);
 module.exports = app;
