4c425f5fe39689d0ce3c6027a48257efd39116ec
SaifHayek <seefhayek4@gmail.com>
2022-08-27 14:52:08 +0300

    make front-end related #3

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+const addBlogBtn = document.querySelector('.add-btn-blog');
+const addCategoryBtn = document.querySelector('.add-btn-category');
+const postsSection = document.querySelector('.posts');
+const addPopUp = document.querySelector('.add-pop-up');
+const closeIcon = document.querySelector('.close-icon');
+
+const toggleClasses = () => {
+  postsSection.classList.toggle('blur');
+  addPopUp.classList.toggle('show-container');
+};
+const removeClasses = () => {
+  postsSection.classList.remove('blur');
+  addPopUp.classList.remove('show-container');
+};
+addBlogBtn.addEventListener('click', toggleClasses);
+postsSection.addEventListener('click', removeClasses);
+closeIcon.addEventListener('click', removeClasses);
