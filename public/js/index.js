8dd90fd5a90497bbbcd79b6123c39a0690b2c2dc
SaifHayek <seefhayek4@gmail.com>
2022-08-27 19:30:44 +0300

    make form for category related #11

--- public/js/index.js
+++ public/js/index.js
@@ -2,20 +2,38 @@ const addBlogBtn = document.querySelector('.add-btn-blog');
 const addCategoryBtn = document.querySelector('.add-btn-category');
 const postsSection = document.querySelector('.posts');
 const addPopUp = document.querySelector('.add-pop-up');
+const addCategoryPopUp = document.querySelector('.add-category-pop-up');
 const closeIcon = document.querySelector('.close-add-icon');
+const closeCategoryIcon = document.querySelector('.close-category-icon');
 
 // Toggle classes
 const toggleClasses = () => {
   postsSection.classList.toggle('blur');
   addPopUp.classList.toggle('show-container');
 };
+const toggleCategoryClasses = () => {
+  postsSection.classList.toggle('blur');
+  addCategoryPopUp.classList.toggle('show-container');
+};
 const removeClasses = () => {
   postsSection.classList.remove('blur');
-  addPopUp.classList.remove('show-container');
+  if (addPopUp.classList.contains('show-container')) {
+    addPopUp.classList.remove('show-container');
+  }
+  if (addCategoryPopUp.classList.contains('show-container')) {
+    addCategoryPopUp.classList.remove('show-container');
+  }
 };
-
+const removeCategoryClasses = () => {
+  postsSection.classList.remove('blur');
+  addCategoryPopUp.classList.remove('show-container');
+};
 // Events
 addBlogBtn.addEventListener('click', toggleClasses);
+addCategoryBtn.addEventListener('click', toggleCategoryClasses);
+
 postsSection.addEventListener('click', removeClasses);
+
 closeIcon.addEventListener('click', removeClasses);
+closeCategoryIcon.addEventListener('click', removeCategoryClasses);
 fetch('/posts').then((data) => data.json()).then((data) => console.log(data));
